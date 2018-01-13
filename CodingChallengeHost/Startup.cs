1c5a6cef3c13186f0ddff770c06740ccb1142c24
Nathan Richards <nathan.richards@infinityworks.com>
2018-01-13 22:05:40 +0000

    Connected to Google for auth

--- CodingChallengeHost/Startup.cs
+++ CodingChallengeHost/Startup.cs
@@ -33,6 +33,12 @@ namespace CodingChallengeHost
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.AddAuthentication().AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+                googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            });
+
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
